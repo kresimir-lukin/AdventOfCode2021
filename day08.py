162727fc2ad28709e1249b6b41a7ddd5c65ebe00
Kresimir Lukin <klukyn@gmail.com>
2021-12-08 23:48:56 +0400

    added comments under day 8 to better solution

--- day08.py
+++ day08.py
@@ -4,36 +4,40 @@ def get_digits_by_length(digits, length):
     return [''.join(sorted(digit)) for digit in digits if len(digit) == length]
 
 def get_digit_mapping(digits):
-    digits2 = get_digits_by_length(digits, 2)
-    digits3 = get_digits_by_length(digits, 3)
-    digits4 = get_digits_by_length(digits, 4)
-    digits5 = get_digits_by_length(digits, 5)
-    digits6 = get_digits_by_length(digits, 6)
-    digits7 = get_digits_by_length(digits, 7)
+    segments2 = get_digits_by_length(digits, 2)
+    segments3 = get_digits_by_length(digits, 3)
+    segments4 = get_digits_by_length(digits, 4)
+    segments5 = get_digits_by_length(digits, 5)
+    segments6 = get_digits_by_length(digits, 6)
+    segments7 = get_digits_by_length(digits, 7)
 
     mapping = {
-        digits2[0]: '1',
-        digits4[0]: '4',
-        digits3[0]: '7',
-        digits7[0]: '8'
+        segments2[0]: '1',
+        segments4[0]: '4',
+        segments3[0]: '7',
+        segments7[0]: '8'
     }
 
-    d3 = next(digit for digit in digits5 if len(set(digit) - set(digits3[0])) == 2)
+    # 3 is the only digit of 5 segments which contains all segments as 7, so diff must be 2
+    d3 = next(digit for digit in segments5 if len(set(digit) - set(segments3[0])) == 2)
     mapping[d3] = '3'
-    digits5.remove(d3)
+    segments5.remove(d3)
 
-    d6 = next(digit for digit in digits6 if set(digits3[0]) - set(digit))
+    # 6 is the only digit of 6 segments which does not have all 3 segments as 7, so there must be diff of 1
+    d6 = next(digit for digit in segments6 if len(set(segments3[0]) - set(digit)) == 1)
     mapping[d6] = '6'
-    digits6.remove(d6)
+    segments6.remove(d6)
 
-    d5, d9 = next((d5, d6) for d6 in digits6 for d5 in digits5 if len(set(d6) & set(d5)) == 5)
+    # 9 have all of the same segments as 5 plus 1 more, so diff of intersection must be 5
+    d5, d9 = next((d5, d6) for d6 in segments6 for d5 in segments5 if len(set(d6) & set(d5)) == 5)
     mapping[d5] = '5'
     mapping[d9] = '9'
-    digits5.remove(d5)
-    digits6.remove(d9)
+    segments5.remove(d5)
+    segments6.remove(d9)
 
-    mapping[digits5.pop()] = '2' 
-    mapping[digits6.pop()] = '0'
+    # remaining numbers of 5 and 6 segments
+    mapping[segments5.pop()] = '2' 
+    mapping[segments6.pop()] = '0'
 
     return mapping
 
